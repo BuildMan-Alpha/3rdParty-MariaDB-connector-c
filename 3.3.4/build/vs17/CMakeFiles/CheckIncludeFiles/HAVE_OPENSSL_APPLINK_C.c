b767932de28f71e2b41bc35ba91a6177dae9fd9e
cian chambliss <cian@alphasoftware.com>
2024-12-20 09:16:45 -0500

    Initial commit

---
+++ 3.3.4/build/vs17/CMakeFiles/CheckIncludeFiles/HAVE_OPENSSL_APPLINK_C.c
@@ -0,0 +1,6 @@
+/* */
+#include <C:/dev/3rdParty/OpenSSL/3.1.1/include/openssl/applink.c>
+
+
+int main(void){return 0;}
+
---
+++ 3.3.4/build/vs17/external/zlib/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/dev/3rdParty/MariaDB/connector-c/3.3.4/external/zlib
+# Build directory: C:/dev/3rdParty/MariaDB/connector-c/3.3.4/build/vs17/external/zlib
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
